997126eb8a9cf7b147c3461fadd3989d156149df
zhanghao <1402425845@qq.com>
2022-04-11 16:21:13 +0800

    添加mybatis-plus和mysql配置

--- src/main/java/com/pccc/sip/ivrtest/Application.java
+++ src/main/java/com/pccc/sip/ivrtest/Application.java
@@ -1,8 +1,12 @@
 package com.pccc.sip.ivrtest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.pccc.sip.ivrtest.mapper")
 @SpringBootApplication
 public class Application {
 

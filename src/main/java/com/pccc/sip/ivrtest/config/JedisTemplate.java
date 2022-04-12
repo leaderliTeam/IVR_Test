80a0cfcc5e2d7d45ab00958fb4ec01927671f26c
dengqiankun <1562548375@qq.com>
2022-04-12 16:19:53 +0800

    执行引擎基础

--- src/main/java/com/pccc/sip/ivrtest/config/JedisTemplate.java
+++ src/main/java/com/pccc/sip/ivrtest/config/JedisTemplate.java
@@ -12,71 +12,58 @@ public class JedisTemplate {
     private JedisPool jedisPool;
 
     public String set(String key, String value) {
-        Jedis jedis = jedisPool.getResource();
-        try {
+        try (Jedis jedis = jedisPool.getResource()) {
             return jedis.set(key, value);
         } catch (Exception e) {
             return "-1";
-        } finally {
-            jedisPool.close();
-
         }
     }
 
     public String get(String key) {
-        Jedis jedis = jedisPool.getResource();
-        try {
+        try (Jedis jedis = jedisPool.getResource()) {
             return jedis.get(key);
         } catch (Exception e) {
             return "-1";
-        } finally {
-            jedis.close();
         }
     }
 
     public Long del(String key) {
-        Jedis jedis = jedisPool.getResource();
-        try {
+        try (Jedis jedis = jedisPool.getResource()) {
             return jedis.del(key);
         } catch (Exception e) {
             return -1L;
-        } finally {
-            jedis.close();
         }
     }
 
     public Boolean exists(String key) {
-        Jedis jedis = null;
-        try {
-            jedis = jedisPool.getResource();
+        try (Jedis jedis = jedisPool.getResource()) {
             return jedis.exists(key);
         } catch (Exception e) {
             return false;
-        } finally {
-            jedis.close();
         }
     }
 
     public Long lpush(String key, String value) {
-        Jedis jedis = jedisPool.getResource();
-        try {
-            return jedis.lpush(key,value);
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.lpush(key, value);
         } catch (Exception e) {
             return -1L;
-        } finally {
-            jedisPool.close();
-
         }
     }
 
     public String rpop(String key) {
-        Jedis jedis = jedisPool.getResource();
-        try {
+        try (Jedis jedis = jedisPool.getResource()) {
             return jedis.rpop(key);
         } catch (Exception e) {
             return "-1";
-        } finally {
-            jedisPool.close();
+        }
+    }
+
+    public Long llen(String key) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.llen(key);
+        } catch (Exception e) {
+            return -1L;
         }
     }
 }
---
+++ src/main/java/com/pccc/sip/ivrtest/engine/EngineRedisQueue.java
@@ -0,0 +1,42 @@
+package com.pccc.sip.ivrtest.engine;
+
+import com.pccc.sip.ivrtest.config.JedisTemplate;
+import com.pccc.sip.ivrtest.entity.ExecuteCase;
+import com.pccc.sip.ivrtest.util.GsonUtil;
+import io.leaderli.litil.meta.Lino;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EngineRedisQueue {
+
+    private static final Logger logger = LoggerFactory.getLogger(EngineRedisQueue.class);
+
+    private static final String EXECUTE_CASES_KEY = "execute_cases";
+
+    @Autowired
+    private JedisTemplate jedisTemplate;
+
+    public void put(List<ExecuteCase> executeCases) {
+        Lino.of(executeCases).get().forEach(executeCase -> {
+            jedisTemplate.lpush(EXECUTE_CASES_KEY, GsonUtil.GsonString(executeCase));
+        });
+    }
+
+    public ExecuteCase poll() {
+        return GsonUtil.GsonToBean(jedisTemplate.rpop(EXECUTE_CASES_KEY), ExecuteCase.class);
+    }
+
+    public boolean exists() {
+        return jedisTemplate.exists(EXECUTE_CASES_KEY);
+    }
+
+    public Long size() {
+        return jedisTemplate.llen(EXECUTE_CASES_KEY);
+    }
+
+}

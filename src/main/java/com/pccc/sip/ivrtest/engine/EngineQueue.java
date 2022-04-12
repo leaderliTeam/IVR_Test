43678fdeafa10253264b35f3e93b95ab148091b1
dengqiankun <1562548375@qq.com>
2022-04-12 11:44:33 +0800

    执行案例基础

--- src/main/java/com/pccc/sip/ivrtest/engine/EngineQueue.java
+++ src/main/java/com/pccc/sip/ivrtest/engine/EngineQueue.java
@@ -1,30 +1,40 @@
 package com.pccc.sip.ivrtest.engine;
 
 import com.pccc.sip.ivrtest.entity.ExecuteCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Component
 public class EngineQueue {
 
-    private static BlockingQueue<ExecuteCase> queue = new LinkedBlockingQueue<>();
+    private static final Logger logger = LoggerFactory.getLogger(EngineQueue.class);
 
-    public void put(ExecuteCase executeCase) {
-        try {
-            queue.put(executeCase);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static final BlockingQueue<ExecuteCase> queue = new LinkedBlockingQueue<>();
+
+    public void put(List<ExecuteCase> executeCases) {
+        executeCases.forEach(executeCase -> {
+            try {
+                queue.put(executeCase);
+            } catch (InterruptedException e) {
+                logger.error(" engineQueue error -> ",e);
+            }
+        });
     }
 
     public ExecuteCase poll() {
-        return queue.poll() == null ? null : queue.poll();
+        return queue.poll();
     }
 
     public boolean checkQueue() {
         return queue.isEmpty();
     }
 
+    public int size(){
+        return queue.size();
+    }
 }
--- src/main/java/com/pccc/sip/ivrtest/processor/EngineProcessor.java
+++ src/main/java/com/pccc/sip/ivrtest/processor/EngineProcessor.java
@@ -1,6 +1,8 @@
 package com.pccc.sip.ivrtest.processor;
 
 import com.pccc.sip.ivrtest.engine.EngineQueue;
+import com.pccc.sip.ivrtest.engine.ExecuteEngine;
+import com.pccc.sip.ivrtest.entity.ExecuteCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +12,17 @@ import javax.annotation.PostConstruct;
 public class EngineProcessor {
 
     @Autowired
-    EngineQueue engineQueue;
+    private EngineQueue engineQueue;
+    @Autowired
+    private ExecuteEngine executeEngine;
 
     @PostConstruct
     public void init() {
         new Thread(() -> {
             while (true) {
                 if (!engineQueue.checkQueue()) {
-                    System.out.println("+++++++" + System.currentTimeMillis());
-                    engineQueue.poll();
+                    ExecuteCase executeCase = engineQueue.poll();
+                    executeEngine.executeCases(executeCase);
                 }
             }
         }).start();

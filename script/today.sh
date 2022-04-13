319dc6c69c50f1ea6d009815a893c4646b281ef7
simolegiue <simolegiue@foxmail.com>
2022-04-13 17:06:23 +0800

    新增建表语句=====

---
+++ script/today.sh
@@ -0,0 +1,15 @@
+cal(){
+   git log --all --shortstat   --author "$1" --after 'yesterday'\
+    | grep "files\? changed" \
+    | awk '{files+=$1; inserted+=$4; deleted+=$6} END \
+           {print "'$1' --> lines inserted:", inserted, "lines deleted:", deleted}'  
+}
+
+
+
+
+
+cal dengqiankun
+cal ZhangHaoSy
+cal wleecc
+cal simolegiue
---
+++ script/yesterday.sh
@@ -0,0 +1,15 @@
+cal(){
+   git log --all --shortstat   --author "$1" --after '2 day ago' --before '1 day ago' \
+    | grep "files\? changed" \
+    | awk '{files+=$1; inserted+=$4; deleted+=$6} END \
+           {print "'$1' --> lines inserted:", inserted, "lines deleted:", deleted}'  
+}
+
+
+
+
+
+cal dengqiankun
+cal ZhangHaoSy
+cal wleecc
+cal simolegiue

8c997dfc37b59b8c129df0f1ff1b76dc588154b9
zhanghao <1402425845@qq.com>
2022-04-14 17:13:14 +0800

    预期规则2

--- src/main/java/com/pccc/sip/ivrtest/pojo/ExpResultRule.java
+++ src/main/java/com/pccc/sip/ivrtest/pojo/ExpResultRule.java
@@ -1,10 +1,14 @@
 package com.pccc.sip.ivrtest.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 
+@TableName("result_and_rule")
 public class ExpResultRule {
-    @TableField(value = "id")
-    private int id;
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
     @TableField(value = "test_case_id")
     private String testCaseId;
     @TableField(value = "order_id")
@@ -14,11 +18,11 @@ public class ExpResultRule {
     @TableField(value = "rule_value")
     private String ruleValue;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
